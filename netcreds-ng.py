dbb08107feee30793c6b4ecfeb84196c2735ec8b
haydena23 <haydena23@up.edu>
2025-10-02 19:28:30 -0500

    Lazy Scapy imports for performance

--- netcreds-ng.py
+++ netcreds-ng.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python
 
 """
 To-Do:
@@ -6,15 +6,18 @@ To-Do:
 * Update function
 * Multiple OS support
 * Packet Parsing
+* Rich for better dashboard
 """
+
+from __future__ import annotations
 
 # import logging
 import argparse
 import sys
 import platform
-# import subprocess
+from subprocess import Popen, PIPE, DEVNULL
 from typing import Optional, Sequence
-from scapy.all import PcapReader, conf
+from os import geteuid
 
 APP_NAME = "netcreds-ng"
 __version__ = "1.0.0"
@@ -27,20 +30,21 @@ ROOT = 0
 def update() -> None:
     """Check for the latest version. Update if requested."""
     
-def interface_finder() -> str:
+def interface_finder() -> bytes | None:
     """Search for a valid interface, depending on the OS"""
     os = platform.system()
     if os == "Linux":
-        # TODO
-        # Code to find interface
-        # return interface
-        pass
+        ip_route = Popen(['/sbin/ip', 'route'], stdout=PIPE, stderr=DEVNULL)
+        for line in ip_route.communicate()[0].splitlines():
+            if b"default" in line:
+                interface = line.split()[4]
+                return interface
     else:
         print(f"[!] Currently only Linux is supported")
         SystemExit(ERROR)
 
 def parse_packet() -> None:
-    pass
+    """Parse a network packet extracted from PCAP"""
 
 def main(argv: Optional[Sequence[str]] = None) -> int:
     parser = argparse.ArgumentParser(
@@ -49,20 +53,15 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
 
     parser.add_argument("-i", "--interface", help="Choose an interface")
     parser.add_argument("-p", "--pcap", help="Parse info from a pcap file")
-    parser.add_argument("-f", "--filterip", help="Do not sniff packets from this IP address")
     parser.add_argument("-v", "--verbose", help="Display entire URLs and POST loads rather than truncating at 100 characters", action="store_true")
-    
-    update_group = parser.add_mutually_exclusive_group()
-    update_group.add_argument("-u", "--update", help="Update to the latest version of netcreds-ng")
+    parser.add_argument("-u", "--update", help="Update to the latest version of netcreds-ng")
 
-    is_no_args_run = (argv is None and len(sys.argv) == 1) or (argv is not None and not argv)
-    if is_no_args_run:
-        parser.print_usage(sys.stderr)
-        print(f"\n[i] No arguemnts provided. Try `{APP_NAME} --help for a list of all options.", file=sys.stderr)
-        return SUCCESS
+    filter_group = parser.add_mutually_exclusive_group()
+    filter_group.add_argument("-f", "--filterip", help="Do not sniff packets from host1,host2,...", type=str)
+    filter_group.add_argument("-F", "--filterfile", help="Do not sniff packets new-line delimeted file", type=str)
     
     args = parser.parse_args(argv)
-   
+
     if args.update:
         try:
             update()
@@ -71,25 +70,37 @@ def main(argv: Optional[Sequence[str]] = None) -> int:
             print(f"[!] Error updating netcreds-ng: {e}", file=sys.stderr)
             return ERROR
 
-    if args.pcap:
+    if args.pcap is not None:
         try:
+            # Lazy import of Scapy for performance
+            from scapy.utils import PcapReader
             for packet in PcapReader(args.pcap):
-                pass
                 # parse_packet(packet)
+                pass
         except IOError as e:
             print(f"[!] Could not open PCAP file: {e}", file=sys.stderr)
             return ERROR
+        except ImportError as e:
+            print(f"[!] Scapy is required to examine PCAP files: {e}", file=sys.stderr)
+            return ERROR
     else:
-        # Uncomment for Linux
-        #if geteuid() is not ROOT:
-        #    print(f"[-] Please run as root or with root privileges")
-        #    return ERROR
+        # Lazy import of Scapy for performance
+        try:
+            from scapy.config import conf
+            from scapy.sendrecv import sniff
+        except ImportError as e:
+            print(f"[!] Scapy is required for live sniffing: {e}", file=sys.stderr)
+            return ERROR
+        
+        if geteuid() is not ROOT:
+           print(f"[!] Please run as root. Current UID: {geteuid()}")
+           return ERROR
+        
         if args.interface:
             conf.iface = args.interface
         else:
             conf.iface = interface_finder()
-        print(f"[*] Using Interface: {conf.iface}")
-        
+        print(f"[*] Using Interface: {conf.iface}") # type: ignore
 
     return SUCCESS
 
