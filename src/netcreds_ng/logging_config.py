e75ef904716a41ba1b49707f5a6e0a509a51bf41
haydena23 <haydena23@up.edu>
2025-10-19 23:01:24 -0500

    Slight adjustment to logging_config

--- src/netcreds_ng/logging_config.py
+++ src/netcreds_ng/logging_config.py
@@ -3,11 +3,13 @@ from __future__ import annotations
 import logging
 
 class LoggingFormatter(logging.Formatter):
-    """Custom logging formatter."""
+    """Custom logging formatter. Handles different formats for INFO and DEBUG."""
     def __init__(self, fmt_debug: str, fmt_info: str):
         super().__init__()
         self.fmt_debug = fmt_debug
         self.fmt_info = fmt_info
+
+        # Default to info format
         self._style._fmt = self.fmt_info
 
     def format(self, record: logging.LogRecord) -> str:
@@ -20,25 +22,28 @@ class LoggingFormatter(logging.Formatter):
 
         result = super().format(record)
 
+        # Restore the original format
         self._style._fmt = original_format
 
         return result
 
 def setup_logging(is_verbose: bool, is_quiet: bool) -> None:
-    """Set up the logging configuration."""
+    """Set up the global logging configuration."""
     fmt_debug = "[%(asctime)s] [DEBUG] %(filename)s:%(lineno)d: %(message)s"
-    fmt_info = "[%(asctime)s] [DEBUG] %(filename)s:%(lineno)d: %(message)s" if is_verbose else "[%(levelname)s] %(message)s"
+    fmt_info = fmt_debug if is_verbose else "[%(asctime)s] [%(levelname)s] %(message)s"
 
     handler = logging.StreamHandler()
     handler.setFormatter(LoggingFormatter(fmt_debug, fmt_info))
 
-    log_level: int = (
-        logging.DEBUG if is_verbose
-        else logging.CRITICAL if is_quiet
-        else logging.INFO
-    )
+    if is_verbose:
+        log_level = logging.DEBUG
+    elif is_quiet:
+        log_level = logging.CRITICAL
+    else:
+        log_level = logging.INFO
 
     root_logger = logging.getLogger()
+    # Clear any existing handlers
     if root_logger.hasHandlers():
         root_logger.handlers.clear()
 
